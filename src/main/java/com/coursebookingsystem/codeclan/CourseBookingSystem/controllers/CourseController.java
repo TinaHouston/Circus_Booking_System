fc3b2515ef7746b5d5c0f367c7531781736f941a
KathleenEdie <kathleenedie@gmail.com>
2021-04-01 14:38:32 +0100

    mvp complete

--- src/main/java/com/coursebookingsystem/codeclan/CourseBookingSystem/controllers/CourseController.java
+++ src/main/java/com/coursebookingsystem/codeclan/CourseBookingSystem/controllers/CourseController.java
@@ -5,9 +5,7 @@ import com.coursebookingsystem.codeclan.CourseBookingSystem.repositories.CourseR
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,10 +15,27 @@ public class CourseController {
     @Autowired
     CourseRepository courseRepository;
 
+//    @GetMapping(value = "/courses")
+//    public ResponseEntity<List<Course>> getAllCourses(){
+//        return new ResponseEntity<>(courseRepository.findAll(), HttpStatus.OK);
+//    }
+
     @GetMapping(value = "/courses")
-    public ResponseEntity<List<Course>> getAllCourses(){
+    public ResponseEntity<List<Course>> getCoursesByFilters(
+            @RequestParam(name="rating", required = false) Integer rating,
+            @RequestParam(name="customer_name", required = false) String name){
+        if(rating != null){
+            return new ResponseEntity<>(courseRepository.findCourseByStarRating(rating), HttpStatus.OK);}
+        if(name != null){
+            return new ResponseEntity<>(courseRepository.findCourseByBookingsCustomerName(name), HttpStatus.OK);
+        }
         return new ResponseEntity<>(courseRepository.findAll(), HttpStatus.OK);
     }
+
+//    @GetMapping(value= "/courses/customers")
+//    public ResponseEntity<List<Course>> getCoursesByCustomer(@RequestParam(name="customer_name") String name){
+//        return new ResponseEntity<>(courseRepository.findCourseByBookingsCustomerName(name), HttpStatus.OK);
+//    }
 
     @GetMapping(value = "/courses/{id}")
     public ResponseEntity getCourse(@PathVariable Long id){
--- src/main/java/com/coursebookingsystem/codeclan/CourseBookingSystem/repositories/CourseRepository.java
+++ src/main/java/com/coursebookingsystem/codeclan/CourseBookingSystem/repositories/CourseRepository.java
@@ -4,6 +4,12 @@ import com.coursebookingsystem.codeclan.CourseBookingSystem.models.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
+
+    List<Course> findCourseByStarRating(int rating);
+
+    List<Course> findCourseByBookingsCustomerName(String name);
 }
